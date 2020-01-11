bf1c2f4387de7b8fc23a05912f177b093be33030
YetAnotherGhost <59511257+YetAnotherGhost@users.noreply.github.com>
2020-01-11 02:37:45 +0100

    [eroprofile] remove unused import
    (Should be squashed into the previous commit)

--- youtube_dl/extractor/eroprofile.py
+++ youtube_dl/extractor/eroprofile.py
@@ -4,10 +4,7 @@ import re
 
 from .common import InfoExtractor
 from ..compat import compat_urllib_parse_urlencode
-from ..utils import (
-    ExtractorError,
-    unescapeHTML
-)
+from ..utils import ExtractorError
 
 
 class EroProfileIE(InfoExtractor):
